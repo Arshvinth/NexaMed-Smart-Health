339631eec66d08461a72474146f6cb19f25be17a
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2026-04-05 14:22:56 +0530

    Update MedicalReportsController.js

--- services/patient-service/src/controllers/MedicalReportsController.js
+++ services/patient-service/src/controllers/MedicalReportsController.js
@@ -1,4 +1,5 @@
-import { uploadMedicalReports, getMedicalReportsByPatient } from '../services/MedicalReportsService.js';
+import { uploadMedicalReports, getMedicalReportsByPatient, updateMedicalReport } from '../services/MedicalReportsService.js';
+import logger from '../utils/Logger.js';
 
 export const uploadMedicalReport = async (req, res) => {
     try {
@@ -28,3 +29,22 @@ export const getMedicalReports = async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 };
+
+export const updateMedicalReports = async (req, res) => {
+    try {
+        const { reportId } = req.params;
+
+        if (req.files && req.files.length > 0) {
+            const updateReport = await updateMedicalReport(reportId, req.files);
+
+            res.status(200).json({
+                message: "Medical Report Updated Successfully",
+                data: updateReport
+            })
+            logger.info(`Medical report with ID ${reportId} updated successfully`);
+        }
+    } catch (err) {
+        logger.error(`Error updating medical report with ID ${reportId}: ${err.message}`);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
