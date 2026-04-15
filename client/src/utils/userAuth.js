bd550879116aeb99124506f3617ffc8271d6ead1
Arshvinth Sakthivel <IT23373402@my.sliit.lk>
2026-04-15 18:20:53 +0530

    Added general user aith helper file in utils

---
+++ client/src/utils/userAuth.js
@@ -0,0 +1,17 @@
+function getAuthHeaders() {  
+  const userDetails = JSON.parse(localStorage.getItem('user'));
+  const token = localStorage.getItem("token");
+
+  const storedUserId = userDetails?.id || localStorage.getItem("x-user-id");
+  const storedRole = userDetails?.role || localStorage.getItem("x-role");
+  const storedVerification = userDetails?.verificationStatus || localStorage.getItem("x-verification-status");
+
+  return {
+    "authorization": `Bearer ${token}`,
+    "x-user-id": storedUserId ,
+    "x-role": storedRole,
+    "x-verification-status": storedVerification
+  };
+}
+
+export { getAuthHeaders };
