d7ed869274bbba5dd39237595f342947394003b2
Arshvinth Sakthivel <IT23373402@my.sliit.lk>
2026-04-16 15:10:39 +0530

    Added patient service docker compose logic and fixed deployment issue

--- services/patient-service/src/config/db.js
+++ services/patient-service/src/config/db.js
@@ -3,7 +3,7 @@ import logger from "../utils/Logger.js";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         logger.info("MongoDB Connected Successfully");
 
     }
